54454efe33e5668a01facaf2e56ae30ec6fcf69a
Rishabh Tanwar <rishabhtanwar2000@gmail.com>
2023-06-18 08:00:22 +0530

    added landing section of FAQ page

--- src/pages/FAQ.js
+++ src/pages/FAQ.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import FAQImage from '../assets/faqImage.svg';
 
 const FAQ = () => {
-	return <div>FAQ</div>;
+	return (
+		<div className='faq-container'>
+			<div className='faq-landing'>
+				<div className='faq-text'>
+					<h1>Got a question?</h1>
+					<h1>Get your answer</h1>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, modi
+						eligendi. Sint ab optio enim?
+					</p>
+				</div>
+				<div className='faq-image'>
+					<img src={FAQImage} alt='' />
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default FAQ;
