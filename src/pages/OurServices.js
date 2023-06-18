c5d7e68ac1a796596396afafd90b96b8bdbf164e
Rishabh Tanwar <rishabhtanwar2000@gmail.com>
2023-06-18 11:38:48 +0530

    added landing section services page

--- src/pages/OurServices.js
+++ src/pages/OurServices.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import ServicesImage from '../assets/servicesImage.png';
 
 const OurServices = () => {
 	return (
 		<div className='services-container'>
-			<div className='services-landing'></div>
+			<div className='services-landing'>
+				<div className='services-text'>
+					<h1>Dedicated Team Business Model</h1>
+					<p>
+						Hire a team of experts selected to meet your business goals. Get
+						full transparency across all development stages and bring your
+						project to success.
+					</p>
+				</div>
+			</div>
 		</div>
 	);
 };
