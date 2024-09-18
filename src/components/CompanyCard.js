24d6a89d3065bcabe64058419ed4a36e4f2fd12e
Rishabh Tanwar <rishabhtanwar2000@gmail.com>
2024-09-18 21:00:34 +0530

    added contact pop on site load

--- src/components/CompanyCard.js
+++ src/components/CompanyCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Logo from '../assets/logoTrans.png';
+import Logo from '../assets/primefileadvisors.png';
 import { BsFillTelephoneFill, BsCircleFill } from 'react-icons/bs';
 import { GrMail } from 'react-icons/gr';
 import { TbWorldWww } from 'react-icons/tb';
